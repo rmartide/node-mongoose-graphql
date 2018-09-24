1713d83724a64ff465375c82e1069be7cffef667
Rodrigo Martín de Lamo <rmartin@devcenter.es>
2018-09-24 13:38:32 +0200

    Added test for getUser

--- server/tests/graphql.test.js
+++ server/tests/graphql.test.js
@@ -5,11 +5,11 @@ const request = require('supertest');
 
 const URL = '/graphql';
 
-beforeEach(function() {
+beforeEach(function () {
     this.timeout(0);
     return User.deleteMany({}).then(() => {
-        var p1 = User.create({name: 'Toñito'});
-        var p2 = User.create({name: 'Juanito'});
+        var p1 = User.create({ name: 'Toñito' });
+        var p2 = User.create({ name: 'Juanito', teams: [{ name: 'Real Madrid', country: 'España' }] });
         return Promise.all([p1, p2]);
     });
 })
@@ -26,7 +26,7 @@ describe('graphql operations', () => {
         `;
         request(app)
             .post(URL)
-            .send({query})
+            .send({ query })
             .expect(200)
             .expect(res => {
                 var user = res.body.data.addUser;
@@ -35,5 +35,49 @@ describe('graphql operations', () => {
             })
             .end(done);
     });
+
+    it('should get a user with only name', (done) => {
+        const query = `{
+            getUser(name: "Toñito" )
+            { name }
+        }
+        `;
+        request(app)
+            .post(URL)
+            .send({ query })
+            .expect(200)
+            .expect(res => {
+                var user = res.body.data.getUser;
+                wish(user.name === "Toñito");
+                wish(user.teams === undefined)
+            })
+            .end(done);
+    });
+
+    it('should get a user with his teams', (done) => {
+        const query = `{
+            getUser(name: "Juanito" )
+            { 
+                name, 
+                teams {
+                    name,
+                    country
+                }
+            }
+        }
+        `;
+        request(app)
+            .post(URL)
+            .send({ query })
+            .expect(200)
+            .expect(res => {
+                var user = res.body.data.getUser;
+                wish(user.name === "Juanito");
+                wish(user.teams.length === 1);
+                wish(user.teams[0].name === 'Real Madrid');
+                wish(user.teams[0].country === 'España');
+            })
+            .end(done);
+    })
 
 })
